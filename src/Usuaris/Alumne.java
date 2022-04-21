9c81fc45f3e67639fc216432023f22cd4ff32a5d
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2022-04-21 12:26:11 +0200

    El mateix fem amb clase Alumne, pero amb els metodes que ens indica l'enunciat.

--- src/Usuaris/Alumne.java
+++ src/Usuaris/Alumne.java
@@ -1,5 +1,31 @@
 package Usuaris;
 
-public class Alumne {
+import modulsUFs.UF;
 
+public class Alumne extends Persona{
+	
+	public int numUFs;
+	UF[] arrUFsMatriculades;
+	
+	public Alumne(String nom, String cognom, int edad) {
+		
+		
+		
+		super.nom=nom;
+		super.cognom=nom;
+		super.edad=edad;
+		
+		arrUFsMatriculades=new UF[20];
+		numUFs=0;
+		
+		
+	}
+	
+	public void matricular(UF Moduls) {
+		
+		arrUFsMatriculades[numUFs]=Moduls;
+		numUFs++;
+		
+		
+	}
 }
