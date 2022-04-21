a34510893c103c8b4e2fd84b33aed5823a805539
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2022-04-21 12:21:41 +0200

    La clase persona es abstracta, d'on deriva Alumne i Docent i comparteixen els seus atributs.

--- src/Usuaris/Persona.java
+++ src/Usuaris/Persona.java
@@ -1,5 +1,58 @@
 package Usuaris;
 
-public class Persona {
+public abstract class Persona {
+	
+	String nom;
+	String cognom;
+	int edad;
+	
+	
+	
+	Persona(){
+		
+	}
 
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+
+	public String getCognom() {
+		return cognom;
+	}
+
+
+
+	public void setCognom(String cognom) {
+		this.cognom = cognom;
+	}
+
+
+
+	public int getEdad() {
+		return edad;
+	}
+
+
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+
+
+	@Override
+	public String toString() {
+		return "Persona [nom=" + nom + ", cognom=" + cognom + ", edad=" + edad + "]";
+	}
+	
 }
