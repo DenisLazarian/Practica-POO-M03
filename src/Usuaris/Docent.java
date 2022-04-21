7ec635a9aaf5c76ae19de3d231fd9f8aa1b5a681
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2022-04-21 12:24:32 +0200

    Designem la clase constructora i els metodes que aplica, en aquest cas la de un impartir clases.

--- src/Usuaris/Docent.java
+++ src/Usuaris/Docent.java
@@ -1,5 +1,29 @@
 package Usuaris;
 
-public class Docent {
+import modulsUFs.Modul;
+
+public class Docent extends Persona {
+	
+	public int numModuls;
+	Modul[] modulsImpartits;
+	
+	public Docent(String nom, String cognom, int edad) {
+		super.nom=nom;
+		super.cognom=cognom;
+		super.edad=edad;
+		
+		modulsImpartits = new Modul[3];
+		numModuls=0;
+	}
+
+
+	
+
+	public void impartir(Modul m) {
+		
+		modulsImpartits[numModuls]=m;
+		numModuls++;
+		
+	}
 
 }
