fbbf5cdb8d74371ddc353461943afe0f23deecac
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2022-04-21 12:34:42 +0200

    Assignem els atributs i funcions per a la clase UF.

--- src/modulsUFs/UF.java
+++ src/modulsUFs/UF.java
@@ -1,5 +1,46 @@
 package modulsUFs;
 
 public class UF {
+	
+	private Modul refModul;
+	
+	private String nom;
+	private int durada;
 
+	
+	
+
+	public UF(Modul m1, int posicio, String nom, int durada) {
+		
+		
+		
+		
+		this.nom=nom;
+		this.durada=durada;
+		
+		
+	}
+
+
+	public String getNom() {
+		return nom;
+	}
+
+
+	
+
+
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+
+
+	public int getDurada() {
+		return durada;
+	}
+
+
+	public void setDurada(int durada) {
+		this.durada = durada;
+	}
 }
