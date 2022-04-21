2f0fc8183e28aa6dd8da57e6f5e924fa54396593
DenisLazarian <denis.lazarian.emanuel@gmail.com>
2022-04-21 12:32:23 +0200

    Aqui designem els atributs les funcions necesaries indicades per l'enunciat per a la clase modul

--- src/modulsUFs/Modul.java
+++ src/modulsUFs/Modul.java
@@ -1,5 +1,45 @@
 package modulsUFs;
 
 public class Modul {
+	
+	String nomModul;
+	int horesTotals;
+	
+	private UF[] arrUFs;
+	
+	public Modul(String nomModul) {
+		this.nomModul= nomModul;
+		arrUFs= new UF[6];
+		horesTotals = 0;
+	}
 
+
+	public String getNomModul() {
+		return nomModul;
+	}
+
+
+	public void setNomModul(String nomModul) {
+		this.nomModul = nomModul;
+	}
+
+	public int getHoresTotals() {
+		return horesTotals;
+	}
+
+
+	public void setHoresTotals(int horesTotals) {
+		this.horesTotals = horesTotals;
+	}
+
+
+	
+	public UF[] getArrUFs() {
+		return arrUFs;
+	}
+
+
+	public void setArrUFs(UF[] arrUFs) {
+		this.arrUFs = arrUFs;
+	}
 }
